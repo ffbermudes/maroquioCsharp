3e5d8937e298eb3b97790bb75f9dc3dc79f50240
FILIPE BERMUDES <ffbermudes19@gmail.com>
2023-03-12 20:01:59 -0300

    atualizando

---
+++ atributosPropriedadesEscopos/Produto.cs
@@ -0,0 +1,23 @@
+using System;
+namespace classes
+{
+	class Produto{
+		private string nome;
+		public string Nome
+		{
+			get
+			{
+				return nome;
+			}
+			set
+			{
+				if (value.Length > 1)
+					nome = value;
+				else
+				{
+					throw new Exception("Nome do produto deve ter pelo menos 2 caracteres.");
+				}
+			}
+		}
+	}
+}
---
+++ atributosPropriedadesEscopos/main.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace classes
+{
+	class Program
+	{
+		static public void Main(string[]args)
+		{
+			Produto p1 = new Produto();
+			Console.Write("Digite um nome: ");
+			p1.Nome = Console.ReadLine();
+			Console.WriteLine($"O nome do produto é {p1.Nome}.");
+			Console.ReadKey();
+		}
+	}
+}
